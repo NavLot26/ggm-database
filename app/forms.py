bf990bcc9fb6018f138a5c7e9ec969df607ad233
ejalbertine <lizzy.albertine@gmail.com>
2026-04-28 13:16:55 -0400

    add forms for tag filtering

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectMultipleField
 from wtforms.validators import DataRequired, Optional
+from SelectMultipleCheckboxesField import SelectMultipleCheckboxesField
 
 # Admin login form
 class LoginForm(FlaskForm):
@@ -9,8 +10,31 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
+class TagSearchForm(FlaskForm):
+    tags=["Direct Service", "Environmental", "Food Insecurity", "Housing Insecurity", "Animals", "Children", "Elderly", "Over 16", "Health/Medical"],
+    include = SelectMultipleCheckboxesField(
+        "Include tags:",
+        choices = tags
+    )
+    include = SelectMultipleCheckboxesField(
+        "Exclude tags:",
+        choices = tags
+    )
+    submit = SubmitField("submit!")
+
 # Form for creating/editing blog posts
 class OrganizationForm(FlaskForm):
     name = StringField('Organization Name', validators=[DataRequired()])
-    description = TextAreaField('Description', validators=[Optional()])
+    tags=["Direct Service", "Environmental", "Food Insecurity", "Housing Insecurity", "Animals", "Children", "Elderly", "Over 16", "Health/Medical"],
+    include = SelectMultipleField(
+        "Include tags:",
+        choices = tags
+    )
+    description = TextAreaField('Description', validators=[DataRequired()])
+    address1 = TextAreaField('Address 1', validators=[DataRequired()])
+    address2 = TextAreaField('Address 2')
+    city = TextAreaField('City', validators=[DataRequired()])
+    state = TextAreaField('State', validators=[DataRequired()])
+    zipcode = TextAreaField('zipcode', validators=[DataRequired()])
+    website = TextAreaField('website', validators=[DataRequired()])
     submit = SubmitField('Submit')
